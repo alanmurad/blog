9e19899f5b2590ada9f78ce8e0075e340cc92686
alanmurad <alan.murad42@gmail.com>
2020-09-22 21:01:13 +0100

    scaffolding article

--- db/migrate/20200922185000_create_articles.rb
+++ db/migrate/20200922185000_create_articles.rb
@@ -1,10 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-      t.string    :title
-      t.text      :body
-      t.datetime  :published_at
-      
+      t.string :title
+      t.text :body
+      t.datetime :published_at
+
       t.timestamps
     end
   end
